eb75bd519007e8406e03db84391453146c5c0dd4
Chris Duncan <chris@zoso.dev>
2025-09-03 18:07:54 -0700

    Add initial logo component.

---
+++ src/app/components/elements/gnault-logo/gnault-logo-element.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { NanoIdenticonComponent } from 'app/components/elements'
+
+describe('NanoIdenticonComponent', () => {
+	let component: NanoIdenticonComponent
+	let fixture: ComponentFixture<NanoIdenticonComponent>
+
+	beforeEach(waitForAsync(() => {
+		TestBed.configureTestingModule({
+			declarations: [NanoIdenticonComponent]
+		})
+			.compileComponents()
+	}))
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(NanoIdenticonComponent)
+		component = fixture.componentInstance
+		fixture.detectChanges()
+	})
+
+	it('should create', () => {
+		expect(component).toBeTruthy()
+	})
+})
---
+++ src/app/components/elements/gnault-logo/gnault-logo-element.component.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common'
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, ViewChild } from '@angular/core'
+import { createIcon } from 'assets/lib/nanoidenticons.min.cjs'
+
+@Component({
+	selector: 'app-nano-identicon',
+	templateUrl: './nano-identicon.component.html',
+	styleUrls: ['./nano-identicon.component.css'],
+	imports: [
+		CommonModule
+	]
+})
+
+export class NanoIdenticonComponent implements OnChanges, AfterViewInit {
+	@Input() accountID: string
+	@Input() scale: string
+	@Input() settingIdenticonsStyle: string
+	@ViewChild('canvasContainer') canvasContainer: ElementRef
+
+	renderedIdenticon = ''
+	imageLoadErrorOccurred = false
+
+	ngOnChanges () {
+		this.renderNanoidenticon()
+	}
+
+	ngAfterViewInit () {
+		this.renderNanoidenticon()
+	}
+
+	renderNanoidenticon () {
+		if (this.canvasContainer == null
+			|| this.settingIdenticonsStyle !== 'nanoidenticons'
+			|| this.renderedIdenticon === this.accountID
+		) {
+			return
+		}
+		this.renderedIdenticon = this.accountID
+		const scale = parseInt(this.scale) * Math.max(1, window.devicePixelRatio)
+		const canvas = createIcon({
+			seed: this.accountID,
+			scale,
+		})
+		const canvasContainerNative = this.canvasContainer.nativeElement
+		while (canvasContainerNative.firstChild) {
+			canvasContainerNative.removeChild(canvasContainerNative.lastChild)
+		}
+		canvasContainerNative.appendChild(canvas)
+	}
+}
