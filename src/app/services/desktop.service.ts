f836a404f6fcd9d11fe2ba7a90d9bdc137830468
Chris Duncan <chris@zoso.dev>
2025-09-15 23:14:42 -0700

    Ignore IPC in browser. Good news, app still loading, generating wallets, deriving accounts, and importing mnemonics.

--- src/app/services/desktop.service.ts
+++ src/app/services/desktop.service.ts
@@ -3,17 +3,17 @@ import { IpcRenderer } from 'electron'
 
 @Injectable({ providedIn: 'root' })
 export class DesktopService {
-	private ipc: IpcRenderer = window.require('electron').ipcRenderer
+	private ipc?: IpcRenderer = window.require?.('electron').ipcRenderer
 
 	constructor () {
 		console.log(this.ipc ? 'IPC loaded' : 'IPC failed to load')
 	}
 
 	on (channel: string, listener): void {
-		this.ipc.on(channel, (_, ...args) => listener(...args))
+		this.ipc?.on(channel, (_, ...args) => listener(...args))
 	}
 
 	send (channel: string, ...args: any[]): void {
-		this.ipc.send(channel, ...args)
+		this.ipc?.send(channel, ...args)
 	}
 }
