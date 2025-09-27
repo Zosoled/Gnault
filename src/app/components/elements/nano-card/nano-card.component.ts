fa7449cbfc4ccf060b154a641ed8f1d91e37bf20
Chris Duncan <chris@zoso.dev>
2025-09-26 23:02:35 -0700

    Clean up nano card backend.

--- src/app/components/elements/nano-card/nano-card.component.ts
+++ src/app/components/elements/nano-card/nano-card.component.ts
@@ -2,29 +2,22 @@ import { AsyncPipe } from '@angular/common'
 import {
 	Component,
 	ElementRef,
-	EventEmitter,
 	HostListener,
-	Output,
-	Renderer2,
 	Signal,
 	ViewChild,
 	computed,
 	inject
 } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { Router, RouterLink } from '@angular/router'
-import { SwUpdate } from '@angular/service-worker'
+import { RouterLink } from '@angular/router'
 import { TranslocoPipe } from '@jsverse/transloco'
 import { AmountSplitPipe, CurrencySymbolPipe, FiatPipe, RaiPipe } from 'app/pipes'
 import {
 	AppSettingsService,
 	NodeService,
-	NotificationsService,
 	PriceService,
-	UtilService,
 	WalletService
 } from 'app/services'
-import { environment } from 'environments/environment'
 import { Wallet } from 'libnemo'
 
 @Component({
@@ -43,56 +36,23 @@ import { Wallet } from 'libnemo'
 	],
 })
 export class NanoCardComponent {
-	private renderer = inject(Renderer2)
-	private router = inject(Router)
-	private updates = inject(SwUpdate)
-
 	private svcAppSettings = inject(AppSettingsService)
-	private svcNotification = inject(NotificationsService)
-	private svcUtil = inject(UtilService)
 	private svcWallet = inject(WalletService)
 
 	svcNode = inject(NodeService)
 	svcPrice = inject(PriceService)
 
-	@HostListener('document:mousedown', ['$event']) onGlobalClick (event): void {
-		if (
-			this.selectButton.nativeElement.contains(event.target) === false &&
-			this.walletsDropdown.nativeElement.contains(event.target) === false
-		) {
-			this.isWalletsDropdownVisible = false
-		}
-	}
-
-	@Output() animatingChanged = new EventEmitter<boolean>()
-	isAnimating = false
-	@Output() expandedChanged = new EventEmitter<boolean>()
-	isExpanded = false
-
 	@ViewChild('selectButton') selectButton: ElementRef
 	@ViewChild('walletsDropdown') walletsDropdown: ElementRef
 
-	canToggleLightMode = true
-	donationAccount = environment.donationAddress
 	isWalletsDropdownVisible = false
 	node = this.svcNode.node
-	searchData = ''
-	stage = environment.production ? '' : 'BETA'
 
 	get balance () {
 		return this.svcWallet.balance
 	}
 	get displayCurrency () {
 		return this.svcAppSettings.settings.displayCurrency
-	}
-	get hasReceivableTransactions () {
-		return this.svcWallet.hasReceivableTransactions()
-	}
-	get innerHeight () {
-		return window.innerHeight
-	}
-	get innerHeightWithoutMobileBar () {
-		return this.innerHeight - this.mobileBarHeight
 	}
 	get isBalanceInitialized () {
 		return this.svcWallet.isBalanceInitialized
@@ -103,24 +63,6 @@ export class NanoCardComponent {
 	get isConfigured () {
 		return this.svcWallet.isConfigured()
 	}
-	get isLocked () {
-		return this.svcWallet.isLocked()
-	}
-	get isProcessingReceivable () {
-		return this.svcWallet.isProcessingReceivable
-	}
-	get lightModeEnabled () {
-		return this.svcAppSettings.settings.lightModeEnabled
-	}
-	get mobileBarHeight () {
-		return window.innerWidth < 940 ? 50 : 0
-	}
-	get receivable () {
-		return this.svcWallet.receivable
-	}
-	get receivableOption () {
-		return this.svcAppSettings.settings.receivableOption
-	}
 	get selectedAccount () {
 		return this.svcWallet.selectedAccount()
 	}
@@ -129,9 +71,6 @@ export class NanoCardComponent {
 	}
 	get selectedWalletName () {
 		return this.svcWallet.walletNames.get(this.selectedWallet?.id) ?? this.selectedWallet?.id ?? ''
-	}
-	get serverAPI () {
-		return this.svcAppSettings.settings.serverAPI
 	}
 	get walletNames () {
 		return this.svcWallet.walletNames
@@ -146,59 +85,13 @@ export class NanoCardComponent {
 		return Number(mod)
 	})
 
-	constructor () {
-		this.router.events.subscribe(() => {
-			this.closeNav()
-		})
-	}
 
-	applySwUpdate () {
-		this.updates.activateUpdate()
-	}
-
-	toggleNav () {
-		this.isExpanded = !this.isExpanded
-		this.onNavExpandedChange()
-	}
-
-	closeNav () {
-		if (this.isExpanded) {
-			this.isExpanded = false
-			this.onNavExpandedChange()
-		}
-	}
-
-	onNavExpandedChange () {
-		this.isAnimating = true
-		this.animatingChanged.emit(true)
-		this.expandedChanged.emit(true)
-		setTimeout(() => {
-			this.isAnimating = false
-			this.animatingChanged.emit(false)
-		}, 350)
-	}
-
-	toggleLightMode () {
-		if (this.canToggleLightMode === false) {
-			return
-		}
-
-		this.canToggleLightMode = false
-		setTimeout(() => {
-			this.canToggleLightMode = true
-		}, 300)
-
-		this.svcAppSettings.setAppSetting('lightModeEnabled', !this.svcAppSettings.settings.lightModeEnabled)
-		this.updateAppTheme()
-	}
-
-	updateAppTheme () {
-		if (this.svcAppSettings.settings.lightModeEnabled) {
-			this.renderer.addClass(document.body, 'light-mode')
-			this.renderer.removeClass(document.body, 'dark-mode')
-		} else {
-			this.renderer.addClass(document.body, 'dark-mode')
-			this.renderer.removeClass(document.body, 'light-mode')
+	@HostListener('document:mousedown', ['$event']) onGlobalClick (event): void {
+		if (
+			this.selectButton.nativeElement.contains(event.target) === false &&
+			this.walletsDropdown.nativeElement.contains(event.target) === false
+		) {
+			this.isWalletsDropdownVisible = false
 		}
 	}
 
@@ -216,40 +109,5 @@ export class NanoCardComponent {
 		this.svcWallet.selectedWallet.set(wallet)
 		this.toggleWalletsDropdown()
 		this.svcWallet.saveWalletExport()
-	}
-
-	performSearch () {
-		const searchData = this.searchData.trim()
-		if (!searchData.length) return
-
-		const isValidNanoAccount =
-			(searchData.startsWith('xrb_') || searchData.startsWith('nano_')) &&
-			this.svcUtil.account.isValidAccount(searchData)
-
-		if (isValidNanoAccount === true) {
-			this.router.navigate(['accounts', searchData])
-			this.searchData = ''
-			return
-		}
-
-		const isValidBlockHash = this.svcUtil.nano.isValidHash(searchData)
-
-		if (isValidBlockHash === true) {
-			const blockHash = searchData.toUpperCase()
-			this.router.navigate(['transaction', blockHash])
-			this.searchData = ''
-			return
-		}
-
-		this.svcNotification.sendWarning(`Invalid nano address or block hash! Please double check your input`)
-	}
-
-	retryConnection () {
-		if (!this.svcAppSettings.settings.serverAPI) {
-			this.svcNotification.sendInfo(`Wallet server settings is set to offline mode. Please change server first!`)
-			return
-		}
-		this.svcWallet.reloadBalances()
-		this.svcNotification.sendInfo(`Attempting to reconnect to nano node`)
 	}
 }
