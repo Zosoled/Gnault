c1c5d18c2c98956537693ef0b98f2185c6813464
Chris Duncan <chris@zoso.dev>
2025-09-27 19:41:57 -0700

    Fix currency pipe.

--- src/app/pipes/currency-symbol.pipe.ts
+++ src/app/pipes/currency-symbol.pipe.ts
@@ -1,11 +1,13 @@
 import { CurrencyPipe } from '@angular/common'
-import { Pipe, PipeTransform } from '@angular/core'
+import { Pipe, PipeTransform, inject } from '@angular/core'
+import { AppSettingsService } from 'app/services'
 
 // Shows the currency symbol ($, BTC, etc) and removes any numeric values
 @Pipe({ name: 'currencySymbol' })
 export class CurrencySymbolPipe extends CurrencyPipe implements PipeTransform {
+	svcAppSettings = inject(AppSettingsService)
 	transform (value: any): any {
-		const currency = super.transform(0, value.toUpperCase(), 'symbol', '1.0-0')
+		const currency = super.transform(0, this.svcAppSettings.settings.displayCurrency.toUpperCase(), 'symbol', '1.0-0')
 		return currency.replace(/[0-9]/g, '')
 	}
 }
