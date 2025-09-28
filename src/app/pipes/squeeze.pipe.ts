85a5f207043620e5855c53032f23c5652295f143
Chris Duncan <chris@zoso.dev>
2025-09-27 23:28:29 -0700

    Define address squeeze separator.

--- src/app/pipes/squeeze.pipe.ts
+++ src/app/pipes/squeeze.pipe.ts
@@ -2,12 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core'
 
 @Pipe({ name: 'squeeze' })
 export class SqueezePipe implements PipeTransform {
-	transform(value: any, args?: any): any {
+	transform (value: any, args?: any): any {
 		const arg = args?.split(',') ?? []
 		const openingChars = parseInt(arg[0], 10) || 10
 		const closingChars = parseInt(arg[1], 10) || 5
 		const firstChars = value.split('').slice(0, openingChars).join('')
 		const lastChars = value.split('').slice(-closingChars).join('')
-		return value.length < openingChars + closingChars ? value : `${firstChars}...${lastChars}`
+		const separator = ' … '
+		return value.length < openingChars + closingChars ? value : `${firstChars}${separator}${lastChars}`
 	}
 }
