1c080245617977dafef0e87d1d2b41c2c9a85496
Chris Duncan <chris@zoso.dev>
2025-10-07 22:02:47 -0700

    Simply nano card fields by calling signals in template directly.

--- src/app/components/elements/nano-card/nano-card.component.ts
+++ src/app/components/elements/nano-card/nano-card.component.ts
@@ -1,18 +1,9 @@
-import {
-	Component,
-	Signal,
-	computed,
-	inject
-} from '@angular/core'
+import { Component, Signal, computed, inject } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { RouterLink } from '@angular/router'
 import { TranslocoDirective } from '@jsverse/transloco'
 import { AmountSplitPipe, FiatPipe, RaiPipe } from 'app/pipes'
-import {
-	AppSettingsService,
-	NodeService,
-	WalletService
-} from 'app/services'
+import { AppSettingsService, NodeService, WalletService } from 'app/services'
 
 @Component({
 	selector: 'app-nano-card',
@@ -30,38 +21,16 @@ import {
 export class NanoCardComponent {
 	private svcAppSettings = inject(AppSettingsService)
 	private svcNode = inject(NodeService)
-	private svcWallet = inject(WalletService)
+
+	svcWallet = inject(WalletService)
 
 	isWalletsDropdownVisible = false
 	node = this.svcNode.node
-
-	get balance () {
-		return this.svcWallet.balance
-	}
-	get isBalanceInitialized () {
-		return this.svcWallet.isBalanceInitialized
-	}
-	get isBalanceUpdating () {
-		return this.svcWallet.isBalanceUpdating
-	}
-	get isConfigured () {
-		return this.svcWallet.isConfigured()
-	}
-	get selectedAccount () {
-		return this.svcWallet.selectedAccount()
-	}
-	get selectedWallet () {
-		return this.svcWallet.selectedWallet()
-	}
-	get selectedWalletName () {
-		return this.svcWallet.walletNames().get(this.selectedWallet?.id) ?? this.selectedWallet?.id ?? ''
-	}
-	get settings () {
-		return this.svcAppSettings.settings()
-	}
+	selectedAccount = computed(() => this.svcWallet.selectedAccount())
+	settings = computed(() => this.svcAppSettings.settings())
 
 	selectedAccountColor: Signal<number> = computed((): number => {
-		const pk = BigInt(`0x${this.selectedAccount?.publicKey ?? 0}`)
+		const pk = BigInt(`0x${this.svcWallet.selectedAccount()?.publicKey ?? 0}`)
 		const mod = pk % 360n
 		return Number(mod)
 	})
